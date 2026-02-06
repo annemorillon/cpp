8d17867fbd2a7812a1bdb619d87ec933088d9327
anne morillon <anmorill@f0r1s10.paris.42.school>
2026-02-06 19:31:51 +0100

    add exo2

--- cpp00/ex02/srcs/Account.cpp
+++ cpp00/ex02/srcs/Account.cpp
@@ -34,9 +34,77 @@ int	Account::getNbWithdrawals(void)
 	return (_totalNbWithdrawals);
 }
 
-void	displayAccountsInfos(void) // a completer
+void	Account::displayAccountsInfos(void) 
 {
-	std::cout << std::endl;
+	std::cout << _displayTimestamp() << std::endl;
+	std::cout << "accounts:";
+	std::cout << this->_nbAccounts;
+	std::cout << ";total:"
+	std::cout << this->_totalAmount;
+	std::cout << ";deposits:"
+	std::cout << this->_totalNbDeposits;
+	std::cout << ";withdrawals:"
+	std::cout << this->_totalNbWithdrawals;
 }
 
-Account( int initial_deposit );
+// a completer //
+
+Account::Account(int initial_deposit)
+{
+	std::cout << _displayTimestamp();
+	std::cout << "index:";
+	std::cout << this->_accountIndex;
+	std::cout << ";amount:";
+	std::cout << initial_deposit;
+	std::cout << ";created";
+	// ajouter le initial deposit
+	// update de l'index ?
+}
+
+Account::~Account( void )
+{
+	std::cout << _displayTimestamp();
+	std::cout << "index:";
+	std::cout << this->_accountIndex;
+	std::cout << ";amount:";
+	std::cout << this->_amount;
+	std::cout << ";closed";
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+	
+}
+
+void	Account::displayStatus( void ) const
+{
+	std::cout << _displayTimestamp();
+	std::cout << "index:";
+	std::cout << this->_accountIndex;
+	std::cout << ";amount:";
+	std::cout << this->_amount;
+	std::cout << ";deposits:";
+	std::cout << this->_nbDeposits;
+	std::cout << ";withdrawals:";
+	std::cout << this->_nbWithdrawals;
+}
+
+static void	Account::_displayTimestamp( void )
+{
+
+}
+
+Account::Account( void )
+{
+	
+}
