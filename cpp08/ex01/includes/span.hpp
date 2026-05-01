2854fd7e59532f951635486e573af68c1e261fa9
Anne <a.morillon101298@gmail.com>
2026-05-01 07:06:53 +0200

    feat(cpp08): begin ex01

---
+++ cpp08/ex01/includes/span.hpp
@@ -0,0 +1,30 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include "iostream"
+
+// Créez une classe Span capable de stocker un maximum de N entiers.
+// N est une variable de type `unsigned int` et sera le seul paramètre passé au constructeur.
+// sont déjà stockés devra lever une exception. S'il n'y a aucun nombre stocké, ou un seul, aucune distance ne peut être trouvée.
+
+class Span
+{
+	public:
+
+		Span();
+		Span(unsigned int N);
+		~Span();
+		Span(Span const& copy);
+		Span	&operator=(Span const& old);
+
+		void	addNumber(int nb); // ajouter un nb a la liste span // jusqu'a N elements exception leve si au dela
+		int		shortestSpan(); //plus courte distance // exception si pas de nombre
+		int		longestSpan(); //plus longue distance // exception si pas de nombre
+
+	private:
+
+		
+
+};
+
+#endif
---
+++ cpp08/ex01/srcs/main.cpp
@@ -0,0 +1,19 @@
+#include "span.hpp"
+
+int main()
+{
+	Span sp = Span(5);
+	
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+	
+	// ajouter un test avec plus de 10 000 nombres
+
+	return (0);
+}
