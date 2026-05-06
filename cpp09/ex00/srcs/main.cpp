d9caccfb2300814f831cbee8c491cbb834ac12bb
Anne Morillon <a.morillon101298@gmail.com>
2026-05-06 11:59:51 +0200

    feat(cpp09): add function openAndCopyFile

--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -1,15 +1,57 @@
+#include "../includes/BitcoinExchange.hpp"
+
+static bool	openAndCopyFile(char *file, std::string& txt)
+{
+	std::string	line;
+
+	std::ifstream fileIn;
+
+	fileIn.open(file);
+	if (!fileIn)
+	{
+		std::cerr << "Error: open fileIn" << std::endl;
+		return (false);
+	}
+	while (1)
+	{
+		txt += line;
+		if (!getline(fileIn, line))
+			break;
+		else
+			txt += '\n';
+	}
+	fileIn.close();
+	return (true);
+}
+
+int main(int ac, char **av)
+{
+	std::string	txt;
+
+	if (ac != 2)
+	{
+		std::cout << "Usage: ./btc [file]" << std::endl;
+		return (1);
+	}
+	if (!openAndCopyFile(av[1], txt))
+		return (1);
+	if (!parsingFile(txt))
+		return (1);
+		// parsing file
+	// trouver la date la + proche dans la map
+	// recuperer la cle associe
+	// afficher la date + " => " + chiffre + " = " + result
+}
+
 // Parsing :
 // Your program must take a file as an argument. 
 // Each line in this file must use the following format: "date | value".
+	// after date check if " | " otherwise INVALID
 // A valid date will always be in the following format: Year-Month-Day.
+	// if (year > xxxx && year < 2027)
+	// if (month >= 01 && month <= 12)
+	// if (day >= 01 && day <= 31) ajouter le month pour 30 ou 31
 // A valid value must be either a float or a positive integer, between 0 and 1000
-
-#include <BitcoinExchange.hpp>
-
-int main(void)
-{
-
-}
 
 // ex00: map
 // ex01: vector
