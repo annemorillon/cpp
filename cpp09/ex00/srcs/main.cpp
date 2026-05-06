6dea6cc599c3536bd1d0380f18d61dd167f543e7
Anne <a.morillon101298@gmail.com>
2026-05-06 09:14:31 +0200

    feat(cpp09): add ex00

---
+++ cpp09/ex00/srcs/main.cpp
@@ -0,0 +1,16 @@
+// Parsing :
+// Your program must take a file as an argument. 
+// Each line in this file must use the following format: "date | value".
+// A valid date will always be in the following format: Year-Month-Day.
+// A valid value must be either a float or a positive integer, between 0 and 1000
+
+#include <BitcoinExchange.hpp>
+
+int main(void)
+{
+
+}
+
+// ex00: map
+// ex01: vector
+// ex02: list + set
