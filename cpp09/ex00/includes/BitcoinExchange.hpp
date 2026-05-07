5ec9133c05ab72d5f28eed98da32a77ec7ae2042
Anne Morillon <a.morillon101298@gmail.com>
2026-05-07 11:17:52 +0200

    feat(cpp09): add checkDate

--- cpp09/ex00/includes/BitcoinExchange.hpp
+++ cpp09/ex00/includes/BitcoinExchange.hpp
@@ -3,7 +3,7 @@
 
 # include	<iostream>
 # include	<fstream>
-
-
+# include	<sstream>
+# include	<string>
 
 #endif
--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -24,6 +24,76 @@ static bool	openAndCopyFile(char *file, std::string& txt)
 	return (true);
 }
 
+static bool	checkYear(std::string& tmp)
+{
+	int	year;
+	std::istringstream ss(tmp);
+	ss >> year;
+	if (ss.fail())
+		throw ;
+	if (year > 1900 && year < 2027)
+		return (false);
+	return (true);
+}
+
+static bool	checkMonth(std::string& tmp)
+{
+	int	month;
+	std::istringstream ss(tmp);
+
+	ss >> month;
+	if (ss.fail())
+		throw ;
+	if (month >= 01 && month <= 12)
+		return (false);
+	return (true);
+}
+
+static bool	checkMonth(std::string& tmp)
+{
+	int	month;
+	std::istringstream ss(tmp);
+
+	ss >> month;
+	if (ss.fail())
+		throw ;
+	if (month >= 01 && month <= 12)
+		return (false);
+	return (true);
+}
+
+static bool	checkDay(std::string& tmp)
+{
+	int	day;
+	std::istringstream ss(tmp);
+
+	ss >> day;
+	if (ss.fail())
+		throw ;
+	if (day >= 01 && day <= 31) //  ajouter le month pour 30 ou 31
+		return (false);
+	return (true);
+}
+
+static bool checkDate(std::string& date)
+{
+	std::string tmp;
+
+	if (date.length() != 10)
+		return (false);
+	if (date[5] != '|' || date[8] != '|')
+		return (false);
+	tmp = date.substr(0, 4);
+	if (!checkYear(tmp))
+		return (false);
+	tmp = date.substr(6, 7);
+	if (!checkMonth(tmp))
+		return (false);
+	tmp = date.substr(9, 10);
+	if (!checkDay(tmp))
+		return (false);
+}
+
 static bool parsingFile(std::string file)
 {
 	return (true);
@@ -58,10 +128,6 @@ int main(int ac, char **av)
 // Your program must take a file as an argument. 
 // Each line in this file must use the following format: "date | value".
 	// after date check if " | " otherwise INVALID
-// A valid date will always be in the following format: Year-Month-Day.
-	// if (year > xxxx && year < 2027)
-	// if (month >= 01 && month <= 12)
-	// if (day >= 01 && day <= 31) ajouter le month pour 30 ou 31
 // A valid value must be either a float or a positive integer, between 0 and 1000
 
 // ex00: map
