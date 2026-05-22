9c12eb2421f1837194a6782ea56e05c5ef00029c
Anne <a.morillon101298@gmail.com>
2026-05-22 10:42:38 +0200

    fix(cpp09): modify creation of pairs with i+2

--- cpp09/ex02/srcs/PmergeMe.cpp
+++ cpp09/ex02/srcs/PmergeMe.cpp
@@ -90,7 +90,9 @@ static std::vector<int>	sequenceJacobsthal(int n)
 		jacob.push_back(next);
 	}
 
+	result.push_back(0);
 	std::vector<int>::iterator it = ++jacob.begin();
+	// est-ce que cela ajoute bien la fin apres le dernier jacobsthal ?
 	while (it != jacob.end() - 1)
 	{
 		int	prev = *it;
@@ -102,14 +104,6 @@ static std::vector<int>	sequenceJacobsthal(int n)
 			result.push_back(i);
 		}
 	}
-	// for(int i = result.back(); i < n; i++)
-	// {
-	// 	std::vector<int>::iterator it = find(result.begin(), result.end(), i);
-	// 	if (it != result.end())
-	// 		;
-	// 	else
-	// 		result.push_back(i);
-	// }
 	return (result);
 }
 
@@ -126,8 +120,8 @@ void	PmergeMe::_sort(std::vector<int>& arr)
 	if (arr.size() % 2 != 0)
 		non_participating.push_back(arr.back());
 
-	long unsigned int i = 0;
-	while (i + 1 < arr.size()) // pourquoi i + 1
+	// creating of pair winner (main) and pending
+	for (int i = 0; i + 1 < (int)arr.size(); i += 2)
 	{
 		int a = arr[i];
 		int b = -1;
@@ -139,34 +133,27 @@ void	PmergeMe::_sort(std::vector<int>& arr)
 			idx_main.push_back(i);
 			pending.push_back(std::min(a, b));
 		}
-		i++;
 	}
 
 	_sort(main);
 
 	std::vector<int> jacobsthal = sequenceJacobsthal((int) pending.size());
 
-	// std::cout << "jacobsthal: ";
-	// print(jacobsthal);
-	std::cout << "main: ";
-	print(main);
-	std::cout << "pending: ";
-	print(pending);
-	std::cout << "\n";
-
-	int value;
-	std::vector<int>::iterator it;
-	int insert_pos;
-	main.insert(main.begin(), pending[0]);
-	for (int i = 0; i < (int)jacobsthal.size() && i < (int)pending.size(); i++)
+	int jaco = jacobsthal.size() - 1;
+	if (jaco > (int) pending.size())
+		jaco = pending.size() - 1;
+	for (int i = jaco; i >= 0; i--)
 	{
+		// insert number of pending with sequence insert based of jacobsthal
 		int idx = jacobsthal[i];
 		if (idx >= (int)pending.size())
 			break;
-		value = pending[idx];
-		it = lower_bound(main.begin(), main.begin() + idx_main[i], value);
+		int value = pending[idx];
+		std::vector<int>::iterator it = lower_bound(main.begin(), main.begin() + idx_main[i], value);
 		main.insert(it, value);
-		insert_pos = std::distance(main.begin(), it);
+		
+		// update pos winner with idx_main
+		int insert_pos = std::distance(main.begin(), it);
 		for (int i = 0; i < (int)pending.size(); i++)
 		{
 			if (idx_main[i] >= insert_pos)
@@ -174,15 +161,13 @@ void	PmergeMe::_sort(std::vector<int>& arr)
 		}
 	}
 
+	// est-ce qu'il y a besoin d'une boucle ? pas sure
 	for (long unsigned int i = 0; i < non_participating.size(); i++)
 	{
 		std::vector<int>::iterator it = lower_bound(main.begin(), main.end(), non_participating[i]);
 		main.insert(it, non_participating[i]);
 	}
 
-	std::cout << "before: ";
-	print(main);
-	std::cout << "\n";
 	arr = main;
 }
 
